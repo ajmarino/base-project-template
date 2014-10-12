456c9f1d8a2088d60a0a597efcc6c4dcff3db1ac
AJ Marino <aj@ajmarino.com>
2014-10-11 23:21:36 -0400

    Fix sassdoc grunt cmd

--- grunt/compile-css.js
+++ grunt/compile-css.js
@@ -1,5 +1,13 @@
 module.exports = function(grunt) {  
 	
-	grunt.registerTask("css", "Compile just the sass source files", ['clean:css', 'sass:dev', 'autoprefixer:dev', 'csso:dev', 'cache-bust:css']);
+	grunt.registerTask("css", "Compile just the sass source files", [
+		'clean:css',
+		'clean:sass',
+		'sass:dev',
+		'autoprefixer:dev',
+		'csso:dev',
+		'cache-bust:css',
+		'sassdoc'
+	]);
 
 };
