a1abc767ddc1b96f75e0aef6f8deaf3f5551ac00
AJ Marino <aj@ajmarino.com>
2015-01-24 23:46:43 -0500

    Adds susy

--- resources/grunt/tasks/compile-css.js
+++ resources/grunt/tasks/compile-css.js
@@ -2,11 +2,11 @@ module.exports = function(grunt) {
 	
 	grunt.registerTask("css", "Compile main sass source files", [
 		'clean:css',
-		'clean:docs',
 		'sass:dev',
 		'autoprefixer:dev',
 		'csso:dev',
 		'cache-bust:css',
+		'shell',
 		'sassdoc'
 	]);
 
