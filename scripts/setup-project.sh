df7a3958e2da1ffb1df57edbe26829bbfa7e7710
AJ Marino <aj@ajmarino.com>
2015-02-17 14:04:41 -0500

    Updates bower components, setup script

--- scripts/setup-project.sh
+++ scripts/setup-project.sh
@@ -1,5 +1,5 @@
 npm init
 bower init
 npm install --save-dev grunt grunt-contrib-clean grunt-contrib-concat grunt-contrib-jshint grunt-contrib-sass grunt-contrib-uglify grunt-contrib-watch grunt-autoprefixer grunt-csso grunt-sassdoc grunt-shell
-bower install --save-dev html5-boilerplate normalize.css normalize-opentype.css jquery bourbon fontawesome bootstrap-sass-official
+bower install -D html5-boilerplate normalize.css normalize-opentype.css jquery bourbon fontawesome bootstrap-sass-official susy
 composer require --dev way/generators barryvdh/laravel-debugbar
