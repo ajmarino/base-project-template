5d8d91883592afb844c81bf92f057b9a9129e614
AJ Marino <aj@ajmarino.com>
2015-05-31 01:19:08 -0400

    Adds Selector_Cache plugin

--- resources/assets/js/admin/admin-app.js
+++ resources/assets/js/admin/admin-app.js
@@ -22,6 +22,36 @@ $(function () {
 	Admin.init();
 });
 
+
+/**
+ * Creates a cache of jQuery selectors for faster processing
+ *
+ * http://ttmm.io/tech/selector-caching-jquery/
+ * https://gist.github.com/jtsternberg/14978579a9edf42ed069
+ */
+function Selector_Cache() {
+	var elementCache = {};
+ 
+	var get_from_cache = function( selector, $ctxt, reset ) {
+ 
+		if ( 'boolean' === typeof $ctxt ) { reset = $ctxt; }
+		var cacheKey = $ctxt ? $ctxt.selector + ' ' + selector : selector;
+ 
+		if ( undefined === elementCache[ cacheKey ] || reset ) {
+			elementCache[ cacheKey ] = $ctxt ? $ctxt.find( selector ) : jQuery( selector );
+		}
+ 
+		return elementCache[ cacheKey ];
+	};
+ 
+	get_from_cache.elementCache = elementCache;
+	return get_from_cache;
+}
+
+
+
+
+
 /**********************************************************************************************
  * Main App
  * *******************************************************************************************/
@@ -36,8 +66,9 @@ var Admin = function () {
 	 * Init the app
 	 */
 	var init = function () {
-		console.log("Init");
+		console.log("Admin Init");
 
+		var cache = new Selector_Cache();
 
 		// Animates body to anchor tag on page
 		$('.js-slide-page').click(function () {
