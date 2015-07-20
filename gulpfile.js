a00b372b2f4ff4fbf91be817291be94b38478e79
AJ Marino <aj@ajmarino.com>
2015-07-19 22:39:54 -0400

    Adds main sass compilation through gulp
    - Adds gulp task to generate main.scss
    - Adds gulp task to generate sassdocs
    - Versions main.scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,60 @@
-//
-// Sets up short paths for bower components 
-//
-var paths = {
+'use strict';
+
+// -----------------------------------------------------------------------------
+// Dependencies
+// -----------------------------------------------------------------------------
+var gulp         = require('gulp');
+var elixir       = require('laravel-elixir');
+var sassdoc      = require('sassdoc');
+var Notification = require('laravel-elixir/ingredients/commands/Notification');
+
+
+
+
+
+// -----------------------------------------------------------------------------
+// Configuration
+// -----------------------------------------------------------------------------
+var sassPaths = {
 	'bootstrap'   : "./bower_components/bootstrap-sass-official/assets/stylesheets",
 	'bourbon'     : "./bower_components/bourbon/app/assets/stylesheets",
 	'breakpoint'  : "./bower_components/compass-breakpoint/stylesheets",
 	'singularity' : "./bower_components/singularity/stylesheets",
 };
-
-
-// 
-// Dependencies
-// 
-var elixir = require('laravel-elixir');
-var gulp   = require('gulp');
+var sassdocInput   = './resources/assets/sass/**/*.scss';
+var sassdocOptions = { dest: './public/sassdoc' };
 
 
 
 
 
+// -----------------------------------------------------------------------------
+// Sass Documentation
+// -----------------------------------------------------------------------------
+gulp.task('sassdoc', function () {
+  return gulp
+    .src(sassdocInput)
+    .pipe(sassdoc(sassdocOptions))
+    .pipe(new Notification().message('Sass Documentation Generated!'))
+    .resume();
+});
+
+
+
+
+
+// -----------------------------------------------------------------------------
+// Main task
+// -----------------------------------------------------------------------------
 elixir(function(mix) {
     mix.sass('main.scss', 'public/css/app.css', {
     	includePaths: [
-    		paths.bootstrap,
-    		paths.bourbon,
-    		paths.breakpoint,
-    		paths.singularity
+    		sassPaths.bootstrap,
+    		sassPaths.bourbon,
+    		sassPaths.breakpoint,
+    		sassPaths.singularity
     	]
-    });
+       })
+       .task('sassdoc')
+       .version('public/css/app.css');
 });
