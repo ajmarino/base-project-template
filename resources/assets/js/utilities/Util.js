1514c309ae9503953990496fbe65e42c53b9b2d5
AJ <anthony@reflexions.co>
2016-10-03 17:00:38 -0400

    feat: Adds js func to grab url params to Util

--- resources/assets/js/utilities/Util.js
+++ resources/assets/js/utilities/Util.js
@@ -2,6 +2,8 @@ const Util = {};
 
 Util.alert = require('./sweetalert');
 
+Util.getUrlParam = require('./get_url_param');
+
 Util.selectorCache = require('./selector_cache');
 
 Util.slidePageTo = require('./slide_page');
